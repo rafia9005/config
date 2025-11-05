6cbd9fad05f8a567b8d4671f359a69846d498ff3
rafia9005 <rafia9005@gmail.com>
2025-11-05 11:56:17 +0700

    update

--- update.sh
+++ update.sh
@@ -1,7 +1,27 @@
-cp -r ~/.config/alacritty ${PWD}
-cp -r ~/.config/bspwm ${PWD}
-cp -r ~/.config/nvim ${PWD}
-cp -r ~/.config/picom ${PWD}
-cp -r ~/.config/rofi ${PWD}
-cp -r ~/.config/sxhkd ${PWD}
-cp -r ~/.config/polybar ${PWD}
+#!/bin/bash
+
+# Direktori tujuan backup (Diasumsikan script ini dijalankan di dalam direktori "config")
+DEST_DIR="${PWD}"
+
+# Direktori sumber konfigurasi (Home Directory)
+SRC_DIR="${HOME}/.config/"
+
+# --- Gunakan rsync untuk menyalin, mengabaikan folder .git ---
+
+# Menyalin folder Alacritty, bspwm, nvim, picom, rofi, sxhkd, polybar, dan dunst
+rsync -av \
+    --exclude '.git' \
+    "${SRC_DIR}alacritty" \
+    "${SRC_DIR}bspwm" \
+    "${SRC_DIR}nvim" \
+    "${SRC_DIR}picom" \
+    "${SRC_DIR}rofi" \
+    "${SRC_DIR}sxhkd" \
+    "${SRC_DIR}polybar" \
+    "${SRC_DIR}dunst" \
+    "${DEST_DIR}"
+
+# Menyalin file libinput-gestures.conf
+cp "${HOME}/.config/libinput-gestures.conf" "${DEST_DIR}"
+
+echo "Backup konfigurasi berhasil diselesaikan. Folder .git telah diabaikan."
